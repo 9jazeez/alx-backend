76d6f90a562046fdc9fc92752f3b515d7a95eaba
Lawal Azeez <9jazeez@gmail.com>
2022-05-04 17:22:56 +0000

    Simple function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" A helper function for getting page number and size"""
+
+def index_range(page, page_size):
+	"""A function that takes in a page 
+	 -page no
+	 -page size
+	 returns a tuple.
+	"""
+	tup = (page, page_size)
+	return tup
